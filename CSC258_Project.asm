1a80b81cd3bdf28aee72d910f2be0b181a4ab922
Simon-Noble <simon.noble@mail.utoronto.ca>
2024-11-19 18:27:44 -0500

    Added and completed the gravity function

--- CSC258_Project.asm
+++ CSC258_Project.asm
@@ -34,12 +34,19 @@ State: .byte 0:1        # This byte tracks whether the next check should be for 
 .text
 main:
 addi $t0 $zero 1 # Temporary testing
-la $t1 State
-la $t2 Board
+la $t1 Board
+addi $t0, $zero, 1
 
+sll $t2, $t0, 4     # Update the offset to be correct
 
-la $a0, Add
+add $t2, $t2, $t1 
+addi $t3, $zero, 4
+sh $t3, 0($t1)
+sh $t3 , 0($t2)
+
+la $a0, Gravity
 jal Access_All
+
 
 
 # ========= Plans For The Main Loop =======
@@ -174,7 +181,82 @@ jr $ra # Exit function
 
 Add:
 # Add one to the value in the memory address in $a1
+
+la $t0, Board
+sub $t1, $a1, $t0   
+addi $t2, $zero, 0xf0 # Does something different to the bottom row
+bge $t1, $t2, Skip
+
+
 lh $t3, 0($a1) 
-addi $t3, $t3, 1
+addi $t3, $t3, 3
 sh $t3, 0($a1) 
+Skip:
 jr $ra
+
+
+Gravity:                # Single argument $a1. Determine if the piece at the position should move down
+                       # and move it if it should be.
+addi $sp, $sp, -4                    # Store Return Address on the stack
+sw $ra, 0($sp)
+
+la $t0, Board
+sub $t1, $a1, $t0   
+addi $t2, $zero, 0xf0   # Check if we are in the bottom row, if so, return
+bge $t1, $t2, Gravity_Return
+
+
+
+
+lh $t0, 0($a1)              # $t0 is the value at the position we care about
+addi $t1, $zero, 3
+
+ble $t0, $t1, Gravity_Return    # Check if the value at the position is <= 3, if so, return
+addi $t8, $zero, 1
+sll $t8, $t8, 4
+add $t8, $t8, $a1           # $t8 is the position of the piece below
+lh $t9, 0($t8)              # $t9 is The value of the position below
+bgtz $t9, Gravity_Return    # Check if the value at the position below it is > 0, if so, return.
+# Check if the value is 6, 8, 11, 13, 16, 18
+addi $t2, $zero, 6
+beq $t0, $t2, Gravity_Right
+addi $t2, $zero, 11
+beq $t0, $t2, Gravity_Right
+addi $t2, $zero, 16
+beq $t0, $t2, Gravity_Right
+addi $t2, $zero, 8
+beq $t0, $t2, Gravity_Left
+addi $t2, $zero, 13
+beq $t0, $t2, Gravity_Left
+addi $t2, $zero, 18
+beq $t0, $t2, Gravity_Left
+j Shift_Down
+# If the value is 6, 11, 16, check the position to the right and down. If it is >0, return
+Gravity_Right:
+addi $t5, $zero, 1
+sll $t5, $t5, 4
+addi $t5,$t5, 2
+add $t3, $t5, $a1 # $t3 is the position we care about
+lh $t4, 0($t3)      # $t4 is the value we care about
+bgtz $t4, Gravity_Return
+j Shift_Down
+# If the value is 8, 13, 18, check the value at the position tot he left and down, if >0, return
+Gravity_Left:
+addi $t5, $zero, 1
+sll $t5, $t5, 4
+addi $t6, $zero, 2
+sub $t5,$t5, $t6
+add $t3, $t5, $a1   # $t3 is the position we care about
+lh $t4, 0($t3)      # $t4 is the value we care about
+bgtz $t4, Gravity_Return
+j Shift_Down
+
+Shift_Down:
+sh $t0, 0($t8)      # Set the piece at the position below to the same as the current piece.
+sh $zero, 0($a1)       # Set the piece at the current position to 0
+# Return
+Gravity_Return:
+lw $ra, 0($sp)      # Read return address from the stack
+addi $sp, $sp, 4
+jr $ra              # Exit function
+
